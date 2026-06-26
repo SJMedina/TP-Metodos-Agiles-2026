e367420a74308f6aff8abfee66a571f3c355cfc3
Giovanni Malatesta <giovamalatesta19@gmail.com>
2026-06-25 22:54:57 -0300

    importo pdfService actualizado desde rama 4

--- src/main/java/com/example/tpmetodosagiles2026/service/PdfService.java
+++ src/main/java/com/example/tpmetodosagiles2026/service/PdfService.java
@@ -2,15 +2,22 @@ package com.example.tpmetodosagiles2026.service;
 
 import com.example.tpmetodosagiles2026.model.Licencia;
 import com.lowagie.text.Document;
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.Rectangle;
 import com.lowagie.text.pdf.PdfWriter;
-import org.springframework.stereotype.Service;
+import com.lowagie.text.Image;
+import java.net.URL;
 
+import org.springframework.stereotype.Service;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+
 
 @Service
 public class PdfService {
@@ -44,17 +51,46 @@ public class PdfService {
 
     private byte[] generarPdfLicencia(Licencia licencia) {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        Document document = new Document();
-        PdfWriter.getInstance(document, out);
+        
+        Rectangle tamañoTarjeta = new Rectangle(242.65f, 153.01f);
+        
+        //seteo de margenes para centrar bien texto
+        Document document = new Document(tamañoTarjeta, 85f, 10f, 30f, 10f);
+        
+        PdfWriter writer = PdfWriter.getInstance(document, out);
 
         document.open();
-        document.add(new Paragraph("LICENCIA DE CONDUCIR MUNICIPAL"));
-        document.add(new Paragraph("Fecha de emision: " + licencia.getFechaEmision().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
-        document.add(new Paragraph("Titular: " + licencia.getTitular()));
-        document.add(new Paragraph("Documento: " + licencia.getNumeroDocumento()));
-        document.add(new Paragraph("Clase: " + licencia.getClase()));
-        document.add(new Paragraph("Vigencia: " + licencia.getVigencia() + " años"));
-        document.add(new Paragraph("Observaciones: " + licencia.getObservaciones()));
+
+        //imagen
+        try {
+            URL imageUrl = getClass().getResource("/images/plantilla_licencia.jpg");
+            if (imageUrl != null) {
+                Image fondo = Image.getInstance(imageUrl);
+                
+                fondo.scaleAbsolute(tamañoTarjeta.getWidth(), tamañoTarjeta.getHeight());
+                fondo.setAbsolutePosition(0, 0);
+                
+                writer.getDirectContentUnder().addImage(fondo);
+            }
+        } catch (Exception e) {
+            System.err.println("No se pudo cargar la imagen de fondo: " + e.getMessage());
+        }
+
+        // datos
+        Font fontNormal = FontFactory.getFont(FontFactory.HELVETICA, 8);
+        Font fontDestacada = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 8);
+
+        //texto ajustado segun margenes de arriba
+        document.add(new Paragraph("Fecha de emisión: " + licencia.getFechaEmision().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")), fontNormal));
+        document.add(new Paragraph("Titular: " + licencia.getTitular(), fontNormal));
+        document.add(new Paragraph("Documento: " + licencia.getNumeroDocumento(), fontNormal));
+        
+        Paragraph clase = new Paragraph("Clase: " + licencia.getClase(), fontDestacada);
+        document.add(clase);
+        
+        document.add(new Paragraph("Vigencia: " + licencia.getVigencia() + " años", fontNormal));
+        document.add(new Paragraph("Observaciones: \n" + licencia.getObservaciones(), fontNormal));
+        
         document.close();
 
         return out.toByteArray();
